e83679132e8847c87773cecd3458f68b06b1d482
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-04-05 23:58:20 +0900

    Improve: フォームの値を取得し表示させるように改良中

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    <h1>Todoリスト</h1>
+  <div id="root">
+    <div class="section_title">
+      <h1>Todoリスト</h1>
+    </div>
+    <form action="#" method="POST" id="form">
+      <input type="text" name="todo_list">
+    </form>
   </div>
 </template>
+
+<script>
+  window.onload = function addElement() {
+    const forms = document.forms;
+    const forms_value = forms[0].todo_list.value;
+    const element = document.createElement("p");
+    element.innerText = forms_value;
+    const parentElement = document.getElementsById("div")[0];
+    parentElement.appendChild(element);
+  }
+</script>
