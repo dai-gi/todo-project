19cde6d300a9e86d852e0016798763b8b39c0017
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-04-06 23:52:06 +0900

    Fix: 解答例にそって修正

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,21 +1,19 @@
 <template>
   <div id="root">
-    <div class="section_title">
-      <h1>Todoリスト</h1>
-    </div>
-    <form action="#" method="POST" id="form">
-      <input type="text" name="todo_list">
+    <h1>Todoリスト</h1>
+    <form>
+      <input name="todo">
     </form>
   </div>
 </template>
 
 <script>
-  window.onload = function addElement() {
-    const forms = document.forms;
-    const forms_value = forms[0].todo_list.value;
-    const element = document.createElement("p");
-    element.innerText = forms_value;
-    const parentElement = document.getElementsById("div")[0];
-    parentElement.appendChild(element);
+  export default {
+    name: 'TodoList',
+    data() {
+      return {
+        todo: ''
+      }
+    }
   }
 </script>
