9d5d1db1c5bb01d79304625689c925bea6ac36dc
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-04-09 00:25:46 +0900

    Add: ElementUIライブラリを追加

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -4,15 +4,20 @@
     <form @submit.prevent="addTodo()">
       <input v-model="todo">
     </form>
-    <ul>
-      <li v-for="(todo, index) in todos" :key="index">
-        {{ todo }}
-        <button @click.prevent="removeTodo(index)">完了</button>
-      </li>
-    </ul>
+    <el-row :gutter="12">
+      <el-col :span="12" v-for="(todo, index) in todos" :key="index">
+        <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
+          <el-row :gutter="12">
+            <el-col :span="21">{{ todo }}</el-col>
+            <el-col :span="3">
+              <el-button @click="removeTodo(index)" type="success" icon="el-icon-check" circle></el-button>
+            </el-col>
+          </el-row>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
-
 <script>
   export default {
     name: 'TodoList',
