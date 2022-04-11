8cf480c0cfda78fd9bd95e3103bf129acaf92d25
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-04-11 21:25:17 +0900

    Add: Github REST APIを使ってissueのTodoリストを実装

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/TodoList.vue')
+  },
+  {
+    path: '/issue-list',
+    name: 'issue-list',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/IssueList.vue')
   }
 ]
 
