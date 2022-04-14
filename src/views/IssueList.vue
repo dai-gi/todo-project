2e057515c9cd3e4fc019d190c7b5915632b6026a
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-04-15 07:04:09 +0900

    Improve: トークンを削除

--- src/views/IssueList.vue
+++ src/views/IssueList.vue
@@ -25,7 +25,7 @@
     headers: {
       'Accept': 'application/vnd.github.v3+json',
       'Content-Type':'application/json',
-      'Authorization': 'token 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0'
+      'Authorization':
     },
   })
 
