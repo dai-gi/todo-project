f11a01f698cac79503c620be2d404fe1a0b1be2d
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2022-04-19 19:02:53 +0900

    Improve: 同じデザインで複数表示されるコンテンツをコンポーネント化

--- src/views/TodosIssues.vue
+++ src/views/TodosIssues.vue
@@ -5,16 +5,7 @@
       <el-input placeholder="todo" v-model="todo"></el-input>
     </form>
     <el-row :gutter="12">
-      <el-col :span="12" v-for="(todo, index) in todos" :key="index">
-        <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
-          <el-row :gutter="12">
-            <el-col :span="21">{{ todo }}</el-col>
-            <el-col :span="3">
-              <el-button @click="removeTodo(index)" type="success" icon="el-icon-check" circle></el-button>
-            </el-col>
-          </el-row>
-        </el-card>
-      </el-col>
+      <TodoItem v-for="( todo, index ) in todos" :key="index" />
       <el-col :span="12" v-for="(issue, index) in issues" :key="issue.id">
         <el-card class="box-card" shadow="hover" style="margin: 5px 0;">
           <el-row :gutter="12">
@@ -30,6 +21,7 @@
 </template>
 <script>
   import axios from 'axios';
+  import TodoItem from '@/components/TodoItem';
 
   const client = axios.create({
     baseURL: `${process.env.VUE_APP_GITHUB_ENDPOINT}`,
@@ -42,6 +34,9 @@
 
   export default {
     name: 'TodosIssues',
+    components: {
+      TodoItem
+    },
     data() {
       return {
         todo: '',
